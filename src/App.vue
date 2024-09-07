2651232f085bfe5b13f15bfaa50eb87b01d0cb82
esraay <36202874+esraay@users.noreply.github.com>
2024-09-07 21:03:12 +0300

    Add new vue project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  text-align: center;
+}
+</style>
